6fb02db05132e18449445a694f1c0752bd0147dd
jess <jesicatorti@gmail.com>
2022-02-25 15:20:33 -0300

    finished

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ function precioConDescuento(precio, descuento)
 {  const porcentajePago = 100 - descuento;
     return (precio * porcentajePago) /100  
 }
+//pasar a ingles
+const cambio = document.getElementById("idioma");
+idioma.addEventListener("click", changeLanguage);
 
+function changeLanguage()
+{ return window.open(["index_i.html"],["Discount Calculator. English version"]);
+      }
+      
+//volver a castellano
+const cambia = document.getElementById("castellano");
+castellano.addEventListener("click", cambioIdioma);
+function cambioIdioma() { return window.open(["index.html"],["Calculador de Descuentos. Versión en Español"]);
+}
 
 function calcularPrecioConDescuento(){
 const inputPrecio = document.getElementById("inputPrecio");
@@ -10,5 +22,15 @@ const inputDescuento = document.getElementById("inputDescuento");
 const precioInicial = parseInt(inputPrecio.value);
 const descuento = parseInt(inputDescuento.value);
 const precioFinal = precioConDescuento(precioInicial, descuento);
-alert("El precio a pagar con el descuento aplicado es de: $" + precioFinal);
+const mostrar = document.getElementById("frase");
+mostrar.innerText  = "El precio a pagar con el descuento aplicado es de: $" + precioFinal;
 }
+function calculateDiscount(){
+    const inputPrecio = document.getElementById("inputPrecio");
+    const inputDescuento = document.getElementById("inputDescuento");
+    const precioInicial = parseInt(inputPrecio.value);
+    const descuento = parseInt(inputDescuento.value);
+    const precioFinal = precioConDescuento(precioInicial, descuento);
+    const mostrar = document.getElementById("frase");
+    mostrar.innerText  = "The final price is: $" + precioFinal;
+    }
